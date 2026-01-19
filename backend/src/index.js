34cd0956cc90ced5918dfb14d5b4cac2ad10725c
Krishbansa <krishbansal001@outlook.com>
2026-01-20 00:22:39 +0530

    Fix Socket.IO and Express CORS for Vercel + Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://your-vercel-domain.vercel.app" ],
+    origin: ["http://localhost:5173","fullstack-chat-app-master-phi.vercel.app"],
     credentials: true,
   })
 );
